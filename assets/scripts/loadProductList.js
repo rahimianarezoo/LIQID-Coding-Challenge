31c9776e656e92b0186885acd43701e2a0d357f3
rahimianarezoo <rahimianarezoo@uni-koblenz.de>
2021-01-22 18:28:58 +0100

    Add javascript functionality

---
+++ assets/scripts/loadProductList.js
@@ -0,0 +1,40 @@
+function loadProductList(productList, wrapperId) {
+    const listContainer = document.getElementById(wrapperId);
+
+    if (listContainer) {
+        listContainer.innerHTML = '';
+
+        productList.forEach(product => {
+            let li = document.createElement('li');
+            li.classList.add('product-list__item');
+
+            let h4 = document.createElement('h4');
+            h4.classList.add('product-list__item__title');
+            
+            let div = document.createElement('div');
+            div.classList.add('product-list__item__badget');
+            if (product.flag === 'deficit') {
+                div.classList.add('product-list__item__badget--red')
+            }
+            
+            let span = document.createElement('span');
+            span.classList.add('product-list__item__badget__percent');
+            
+            let a = document.createElement('a');
+            a.classList.add('product-list__item__link');
+
+            h4.innerHTML = product.productTitle;
+            span.innerHTML = product.percent;
+            div.innerHTML = product.amount;
+            div.appendChild(span);
+            a.innerHTML = 'Explore';
+
+            li.appendChild(h4);
+            li.appendChild(div);
+            li.appendChild(a);
+            listContainer.appendChild(li);   
+        });
+    } else {
+        throw `${wrapperId} is not defined`
+    }
+}
---
+++ data.js
@@ -0,0 +1,33 @@
+var productList = [
+    {
+        productTitle: 'LIQID Wealth', 
+        amount: '500.000 €',
+        flag: 'surplus',
+        percent: '25.3%'
+    },
+    {
+        productTitle: 'LIQID Cash', 
+        amount: '260.000 €',
+        flag: 'surplus',
+        percent: '15.3%'
+    },
+    {
+        productTitle: 'LIQID Venture', 
+        amount: '928.000 €',
+        flag: 'deficit',
+        percent: '0.13%'
+    },
+    {
+        productTitle: 'LIQID Private Equity', 
+        amount: '850.000 €',
+        flag: 'surplus',
+        percent: '50.8%'
+    },
+    {
+        productTitle: 'LIQID Real State', 
+        amount: '799.000 €',
+        flag: 'surplus',
+        percent: '25.6%'
+
+    }
+];
